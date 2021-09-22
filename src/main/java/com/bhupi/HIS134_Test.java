aadb5ffbefd266f876ee7cd8d0392774103de48a
Bhupendra Singh <bhupi29491@gmail.com>
2021-09-22 08:28:22 +0530

    Added the feature-2021 Changes

--- src/main/java/com/bhupi/HIS134_Test.java
+++ src/main/java/com/bhupi/HIS134_Test.java
@@ -5,5 +5,9 @@ public class HIS134_Test {
 	public static void main(String[] args) {
 		System.out.println("Added & completed all the changes related JIRA:HIS-134");
 	}
+	
+	public void welcomeMessage(){
+		System.out.println("This is just for understanding the GIT Branching concept.");
+	}
 
 }
